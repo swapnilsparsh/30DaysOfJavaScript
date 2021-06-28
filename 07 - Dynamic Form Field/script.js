55d02181cfdfd9a080d23f28d2ea8ef48da58e92
swapnilsparsh <arezona.lucky55@gmail.com>
2021-06-28 23:52:47 +0530

    Day 07 - Dynamic Form Field

---
+++ 07 - Dynamic Form Field/script.js
@@ -0,0 +1,20 @@
+var survey_options = document.getElementById('surve_options');
+var add_more_fields = document.getElementById('add_more_fields');
+var remove_fields = document.getElementById('remove_fields');
+
+add_more_fields.onclick = function(){
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'survey_options[]');
+    newField.setAttribute('class', 'survey_options');
+    newField.setAttribute('siz', 50);
+    newField.setAttribute('placeholder', 'Another Field');
+    survey_options.appendChild(newField);
+}
+
+remove_fields.onclick = function(){
+    var input_tags = survey_options.getElementsByTagName('input');
+    if(input_tags.length > 2){
+        survey_options.removeChild(input_tags[(input_tags.length)-1]);
+    }
+}
