9099f3e45f28197e25d6783810073346ba169975
swapnilsparsh <arezona.lucky55@gmail.com>
2021-07-11 20:07:53 +0530

    Day 03 - Random Color Generator

--- 03 - Random Color Generator/main.js
+++ 03 - Random Color Generator/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
     addColor();
   };
   
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 9; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     document.querySelector('.container').appendChild(box);
