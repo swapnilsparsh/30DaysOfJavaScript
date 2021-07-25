dd04082cb1bc72774eb32c968ff299555e1b660c
swapnilsparsh <arezona.lucky55@gmail.com>
2021-07-25 16:41:34 +0530

    Day 25 - Weather App

--- 25 - Weather App/script.js
+++ 25 - Weather App/script.js
@@ -27,14 +27,9 @@ window.addEventListener('load', () => {
           const { temp, feels_like } = data.main;
           const place = data.name;
           const { description, icon } = data.weather[0];
-          const { sunrise, sunset } = data.sys;
 
-          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
           const fahrenheit = (temp * 9) / 5 + 32;
-
-          // Converting Epoch(Unix) time to GMT
-          const sunriseGMT = new Date(sunrise * 1000);
-          const sunsetGMT = new Date(sunset * 1000);
 
           // Interacting with DOM to show data
           iconImg.src = iconUrl;
@@ -42,8 +37,6 @@ window.addEventListener('load', () => {
           desc.textContent = `${description}`;
           tempC.textContent = `${temp.toFixed(2)} °C`;
           tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-          sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
-          sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
         });
     });
   }
