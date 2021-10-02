d9bb8a28f9d7ec338dd9db865098668b4623fd94
Hetu Patel <hetu200211@gmail.com>
2021-10-02 22:30:42 +0530

    Added Decimal to Binary,hexadecimal,octal in 35 closes https://github.com/swapnilsparsh/30DaysOfJavaScript/issues/25

--- 35 - Decimal To Binary/script.js
+++ 35 - Decimal To Binary/script.js
@@ -1,23 +1,71 @@
-let input = document.getElementById('input_number');
-let output = document.getElementById('output_number');
+let input = document.getElementById("input_number");
+let output = document.getElementById("output_number");
+let select = document.getElementById("select");
+var into = document.getElementById("into");
 
 
-function calculate(){
-    let number = input.value;
-    if(isNaN(number) || number==""){
-        alert("given value is not a number");
-        output.value = "";
+function changeinto(){
+    into.innerHTML = select.value;
+}
+changeinto();
+function calculate() {
+  let number = input.value;
+  if (isNaN(number) || number == "") {
+    alert("given value is not a number");
+    output.value = "";
+  }
+  if (select.value == "Binary") {
+    if (number == 0) {
+      output.value = 0;
+    } else {
+      let response = "";
+      while (number > 0) {
+        response = (number % 2) + response;
+        number = Math.floor(number / 2);
+      }
+      output.value = response;
     }
-    if(number==0){
-        output.value = 0;
-    }
-    else{
-        let response = "";
-        while(number>0){
-            response=number%2 + response;
-            number=Math.floor(number/2);
+  } else if (select.value == "Hexadecimal") {
+    if (number == 0) {
+      output.value = 0;
+    } else {
+      let hexa = [];
+      let r,
+        t,
+        i,
+        j = 0;
+      t = number;
+      while (t > 0) {
+        r = t % 16;
+        if (r < 10) {
+          hexa[j++] = 48 + r;
+        } else {
+          hexa[j++] = 55 + r;
         }
-        output.value = response;
+        t = parseInt(t / 16);
+      }
+      let char = "";
+      for (i = j - 1; i >= 0; i--) {
+        char += String.fromCharCode(hexa[i]);
+      }
+      output.value = char;
     }
-
+  } else if (select.value == "Octal") {
+    if (number == 0) {
+      output.value = 0;
+    } else {
+      let r,
+        t,
+        i = 1,
+        j = 0;
+      t = number;
+      while (t != 0) {
+        r = t % 8;
+        j += i * r;
+        i = i * 10;
+        t = parseInt(t / 8);
+      }
+      output.value = j;
+    }
+  }
 }
