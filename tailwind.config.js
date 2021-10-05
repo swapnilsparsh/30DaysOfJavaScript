493cbd7d8c36b114a8200d23b688ac11a2a911d5
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-10-04 20:17:14 -0500

    Create the entry config for TailwindCSS without any bundling tool.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
