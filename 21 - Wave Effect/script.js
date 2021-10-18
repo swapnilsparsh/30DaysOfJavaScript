33cd42c38b2d7bf90456819a3979bc246d16efdc
seanysean <seanysean700@gmail.com>
2021-10-18 20:33:53 -0300

    Make random wave button only produce one wave

--- 21 - Wave Effect/script.js
+++ 21 - Wave Effect/script.js
@@ -1,10 +1,8 @@
 const container = document.getElementById('container');
-const autoWavesButton = document.getElementById('toggleWaves');
+const randomWaveButton = document.getElementById('randomWave');
 const circlesArr = [];
 let rows = 15;
 let cols = 15;
-
-let autoWaves = false;
 
 // create circles
 for(let i=0; i<cols; i++) {
@@ -26,33 +24,24 @@ circlesArr.forEach((cols, i) => {
 });
 
 function growCircles(i, j) {
-	if(circlesArr[i] && circlesArr[i][j]) {
-		if(!circlesArr[i][j].classList.contains('grow')) {
-			circlesArr[i][j].classList.add('grow');
-			setTimeout(() => {
-				growCircles(i-1, j)
-				growCircles(i+1, j)
-				growCircles(i, j-1)
-				growCircles(i, j+1)
-			}, 100)
-			
-			setTimeout(() => {
-				circlesArr[i][j].classList.remove('grow');
-			}, 300);
-		}
+	if(circlesArr[i] && circlesArr[i][j] && !circlesArr[i][j].classList.contains('grow')) {
+		circlesArr[i][j].classList.add('grow');
+		setTimeout(() => {
+			growCircles(i-1, j)
+			growCircles(i+1, j)
+			growCircles(i, j-1)
+			growCircles(i, j+1)
+		}, 100)
+		
+		setTimeout(() => {
+			circlesArr[i][j].classList.remove('grow');
+		}, 300);
 	}
 }
 
-function makeRandomWaves() {
-	if (autoWaves) {
-		const randomCoords = [Math.floor(Math.random() * cols),Math.floor(Math.random() * cols)];
-		growCircles(randomCoords[0],randomCoords[1]);
-	}
+function makeRandomWave() {
+	const randomCoords = [Math.floor(Math.random() * cols),Math.floor(Math.random() * cols)];
+	growCircles(randomCoords[0],randomCoords[1]);
 }
 
-setInterval(makeRandomWaves,250);
-
-autoWavesButton.addEventListener('click',()=>{
-	autoWaves = !autoWaves;
-	autoWavesButton.innerHTML = `Turn ${autoWaves ? 'off' : 'on'} Random Waves`;
-});
+randomWaveButton.addEventListener('click',makeRandomWave);
