4b35a25959cdc015c711c290c09ee6dc2637bf53
swapnilsparsh <arezona.lucky55@gmail.com>
2021-12-24 23:08:23 +0530

    Dark Mode Added

--- 30DaysOfJavaScript/script.js
+++ 30DaysOfJavaScript/script.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() >= 100) { 
-            $('.scrollToTop').fadeIn(); 
-        } else { 
-            $('.scrollToTop').fadeOut(); 
-        } 
-    }); 
-    $('.scrollToTop').click(function(e){ 
-        e.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, 500); 
-        return false; 
-    }); 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) {
+      $(".scrollToTop").fadeIn();
+    } else {
+      $(".scrollToTop").fadeOut();
+    }
   });
+  $(".scrollToTop").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var button = document.getElementById("toggle");
+  if (button.innerHTML == "🌙") {
+    button.innerHTML = "☀️";
+  } else {
+    button.innerHTML = "🌙";
+  }
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
