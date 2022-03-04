8759b7e47caacdb481b7caef1f70b55f6eac6365
vanshi1405 <vanshi1405shah@gmail.com>
2022-03-05 00:33:23 +0530

    To Do List:-> style.css change

--- 29 - To Do List/script.js
+++ 29 - To Do List/script.js
@@ -3,6 +3,7 @@ var myNodelist = document.getElementsByTagName('LI');
 var i;
 for (i = 0; i < myNodelist.length; i++) {
     var division = document.createElement('div');
+    
     var span = document.createElement('SPAN');
     var txt = document.createTextNode('\u00D7');
     var editSpan = document.createElement('SPAN');
