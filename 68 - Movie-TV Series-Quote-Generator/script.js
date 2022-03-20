425ce2571213b8cb2ea034fff49c6393dbd7dbc8
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-03-20 23:07:11 +0530

    Add files via upload

---
+++ 68 - Movie-TV Series-Quote-Generator/script.js
@@ -0,0 +1,17 @@
+const getNextQuote = async() =>
+{
+  var url = "https://raw.githubusercontent.com/msramalho/json-tv-quotes/master/quotes.json";
+  const response = await fetch(url);
+  const totalQuotes = await response.json();
+  const index = Math.floor(Math.random()*totalQuotes.length);
+  const quo=totalQuotes[index].quote;
+  const auth=totalQuotes[index].author;
+  const sour=totalQuotes[index].source;
+  quote.innerHTML=""+quo;
+  author.innerHTML="~ "+auth;
+  source.innerHTML=""+sour;
+  tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth+"|"+source;
+
+}
+
+getNextQuote();
