f81437bf37fbe7d31b1f5783cc71fbcd315f59c1
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-03 08:41:36 -0700

    :memo: comment added

--- 97-Catch Me If You Can/script.js
+++ 97-Catch Me If You Can/script.js
@@ -3,6 +3,7 @@ var box = document.getElementById("box");
 var viewWidth = window.innerWidth;
 var viewHeight = window.innerHeight;
 
+// Updates the viewport height and width dynamically
 window.addEventListener("resize", function(event) {
     viewWidth = window.innerWidth;
     viewHeight = window.innerHeight;
