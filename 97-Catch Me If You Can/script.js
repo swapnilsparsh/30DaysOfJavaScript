19c272fd25ecf143e6d74502ecb4e6aa479a01c6
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-03 08:41:10 -0700

    :heavy_plus_sign: event listener added

--- 97-Catch Me If You Can/script.js
+++ 97-Catch Me If You Can/script.js
@@ -2,3 +2,8 @@ var box = document.getElementById("box");
 
 var viewWidth = window.innerWidth;
 var viewHeight = window.innerHeight;
+
+window.addEventListener("resize", function(event) {
+    viewWidth = window.innerWidth;
+    viewHeight = window.innerHeight;
+});
