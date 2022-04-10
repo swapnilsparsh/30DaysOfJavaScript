6818729698586515d6f1a4ef8558780a7678faf7
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-10 06:41:53 -0700

    :memo: variable created

--- 99-GitHub/script.js
+++ 99-GitHub/script.js
@@ -1,1 +1,5 @@
 const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
