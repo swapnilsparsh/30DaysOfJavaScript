03787f56dd76d6c88c32fe29c53656c52b5fca5b
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-10 06:42:31 -0700

    :memo: default page added

--- 99-GitHub/script.js
+++ 99-GitHub/script.js
@@ -3,3 +3,14 @@ const APIURL = "https://api.github.com/users/";
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+
+getUser("iamrahulmahato");
+
+async function getUser(username) {
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+
+    getRepos(username);
+}
