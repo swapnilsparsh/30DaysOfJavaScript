d064cc30f412f14ea30ec30a487fe0985e5e49b0
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2022-05-04 16:55:34 +0530

    3d environment

---
+++ 159 - 3D Environmet/script.js
@@ -0,0 +1,4 @@
+// that's one steeep cliff face
+// in episode 3 of jojo's bizarre adventure, there's this really short scene when jojo arrives at london and the city is introduced in a similar way. 
+// This method is pretty common in many animated shows. 
+// I got lazy and stopped making my own art assets and put this together with images found from around the web -_-
