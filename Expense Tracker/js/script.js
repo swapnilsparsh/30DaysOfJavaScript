8529b0041e46d2cd09f8d1bd83881d639f70efe6
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-09 01:48:01 -0700

    :memo: update balance feat added

--- Expense Tracker/js/script.js
+++ Expense Tracker/js/script.js
@@ -64,3 +64,23 @@ function addTransactionDOM(transaction) {
 
     list.appendChild(item);
 }
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+
+    balance.innerText = `${total}`;
+    money_plus.innerText = `${income}`;
+    money_minus.innerText = `${expense}`;
+}
