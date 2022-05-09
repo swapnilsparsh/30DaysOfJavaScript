02d09f744e3d04a9d82f1a4803b7d4fceb614594
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-09 01:43:55 -0700

    :memo: var created

---
+++ Expense Tracker/js/script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
