dd5d67f32157a55c6046ae1433d554f7b8bd7daa
iamrahulmahato <rahul25mahato@gmail.com>
2022-05-09 01:47:23 -0700

    :sparkles: adding transaction dom list added

--- Expense Tracker/js/script.js
+++ Expense Tracker/js/script.js
@@ -43,3 +43,24 @@ function addTransaction(e) {
 function generateID() {
     return Math.floor(Math.random() * 100000000);
 }
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+
+
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+    list.appendChild(item);
+}
