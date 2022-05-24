e61e58042eb0be8c977cf50ce5b2f5da182adb1d
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2022-05-24 23:03:20 +0530

    Fraction Calculator

---
+++ 190 - Fraction Calculator/script.js
@@ -0,0 +1,16 @@
+var x = document.getElementById('testdiv');
+var y = document.getElementById('testInput');
+function testFunction()
+math.randomNumber(1, 5);
+  if(y.innerHTML = 0)  {
+    document.getElementById('testInput').innerHTML = "Test 1";
+  }
+  else if(y.value = 1)  {
+    document.getElementById('testInput').innerHTML = "Test 2";
+  }
+  else if(y.value = 2)  {
+    document.getElementById('testInput').innerHTML = "Test 3";
+  }
+  else(y.value = 3)  {
+    document.getElementById('testInput').innerHTML = "Test 4";
+  }
