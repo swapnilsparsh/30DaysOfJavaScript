c804ddd9f756a100c7b33554c5a990b00c4b2da1
gloirekiba <gloirekiba21@gmail.com>
2022-10-18 01:25:01 +0200

    ♻️ Refactor code Pojet Digital Clock

--- 04 - Digital Clock/script.js
+++ 04 - Digital Clock/script.js
@@ -1,30 +1,31 @@
-function clock(){
-    
-    let hour = document.getElementById('hour');
-    let minute = document.getElementById('minute');
-    let seconds = document.getElementById('seconds');
-    let ampm = document.getElementById('ampm');
-    
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    var am = 'AM' ;
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
 
-    // Conver 24 hour time to 12 hour format with AM/PM Indicator
-    if (h > 12){
-        h = h - 12;
-        var am = 'PM' 
-    }
-
-    // Add 0 to the begining of number if less than 10
-    h = ( h < 10 ) ? '0' + h : h ;
-    m = ( m < 10 ) ? '0' + m : m ;
-    s = ( s < 10 ) ? '0' + s : s ;
-
-    hour.innerHTML = h;
-    minute.innerHTML = m;
-    seconds.innerHTML = s;
-    ampm.innerHTML = am;
+// Add 0 to the begining of number if less than 10
+function formatTime(time) {
+  return time.toString().padStart(2, "0");
 }
 
-var interval = setInterval(clock, 1000);
+function clock() {
+  const date = new Date();
+
+  let h = formatTime(date.getHours());
+  let m = formatTime(date.getMinutes());
+  let s = formatTime(date.getSeconds());
+  let am = "AM";
+
+  // Conver 24 hour time to 12 hour format with AM/PM Indicator
+  if (h > 12) {
+    h = h - 12;
+    am = "PM";
+  }
+
+  hour.textContent = h;
+  minute.textContent = m;
+  seconds.textContent = s;
+  ampm.textContent = am;
+}
+
+setInterval(clock, 1000);
