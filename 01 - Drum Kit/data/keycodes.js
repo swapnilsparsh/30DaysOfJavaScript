db9b0721fe0f8d72ada77c29f333dadd3c89a1ac
gloirekiba <gloirekiba21@gmail.com>
2022-10-18 23:26:56 +0200

    🚚 add keycodes ressource

---
+++ 01 - Drum Kit/data/keycodes.js
@@ -0,0 +1,13 @@
+const keyCodes = [
+  { key: "a", sound: "clap" },
+  { key: "s", sound: "hihat" },
+  { key: "d", sound: "kick" },
+  { key: "f", sound: "openhat" },
+  { key: "g", sound: "boom" },
+  { key: "h", sound: "ride" },
+  { key: "j", sound: "snare" },
+  { key: "k", sound: "tom" },
+  { key: "l", sound: "tink" },
+];
+
+export default keyCodes;
