faab286ab615bc9a98599d1972c563f5c1dd505f
gloirekiba <gloirekiba21@gmail.com>
2022-10-18 23:29:23 +0200

    ♻️ refactor code

--- 01 - Drum Kit/main.js
+++ 01 - Drum Kit/main.js
@@ -1,47 +1,13 @@
-const keys = Array.from(document.querySelectorAll('.key'));
+import keyCodes from "./data/keycodes.js";
 
-const keyCode = {
-    A: 65,
-    S: 83,
-    D: 68,
-    F: 70,
-    G: 71,
-    H: 72,
-    J: 74,
-    K: 75,
-    L: 76,
-};
+const audio = new Audio();
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
-
-function playSound(e) {
-    const audio = document.querySelector(
-        `audio[data-key="${e.keyCode || keyCode[e.target.innerHTML]}"]`
-    );
-
-    const key = document.querySelector(
-        `div[data-key="${e.keyCode || keyCode[e.target.innerHTML]}"]`
-    );
-
-    if (!audio) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-}
-
-
-
-
-keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playSound);
-
-keys.forEach((key) =>
-    key.addEventListener('click', (e) => {
-        playSound(e);
-    })
-);
+window.addEventListener("keydown", async function ({ key }) {
+  const sound = keyCodes.find(
+    (keyCode) => keyCode.key.toLowerCase() === key.toLowerCase()
+  )?.sound;
+  if (sound) {
+    audio.src = `sounds/${sound}.wav`;
+    await audio.play();
+  }
+});
