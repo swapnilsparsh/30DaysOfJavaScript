9e439b11e5617b8ccbe495234fcc6d4c7c938f3b
Shaurya Kushwaha <shaurya08k@gmail.com>
2022-10-23 19:56:18 +0530

    Added graph

--- 25 - Weather App/script.js
+++ 25 - Weather App/script.js
@@ -7,10 +7,9 @@ const loc = document.querySelector('#location');
 const tempC = document.querySelector('.c');
 const tempF = document.querySelector('.f');
 const desc = document.querySelector('.desc');
+let dat,lat,long;
 
 window.addEventListener('load', () => {
-  let long;
-  let lat;
       fetch(base)
         .then((response) => {
           return response.json();
@@ -67,7 +66,12 @@ function getWeatherReport(city){
   fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
   .then(weather => {
       return weather.json();
-  }).then(showWeatherReport);
+  }).then((data) => {
+    showWeatherReport(data);
+    lat = data.coord.lat;
+    long = data.coord.lon;
+    fetching();
+  });
 }
 
 function showWeatherReport(weather){
@@ -128,7 +132,93 @@ function dateManage(dateArg){
 
 }
 
+function fetching(){
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${weatherApi.key}&units=metric`)
+  .then(response => response.json())
+  .then((data) =>{
+      dat = data;
+      google.charts.load('current',{packages:['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+  });
+}
 
+function drawChart() {
+  let unix_time_0 = dat.hourly[0].dt;
+  let unix_time_1 = dat.hourly[1].dt;
+  let unix_time_2 = dat.hourly[2].dt;
+  let unix_time_3 = dat.hourly[3].dt;
+  let unix_time_4 = dat.hourly[4].dt;
+  let unix_time_5 = dat.hourly[5].dt;
+  let unix_time_6 = dat.hourly[6].dt;
+  let unix_time_7 = dat.hourly[7].dt;
+  let unix_time_8 = dat.hourly[8].dt;
+  let unix_time_9 = dat.hourly[9].dt;
+  let unix_time_10 = dat.hourly[10].dt;
+  let unix_time_11 = dat.hourly[11].dt;
+  
+  var hour0 = new Date(unix_time_0 * 1000);
+  var hour1 = new Date(unix_time_1 * 1000);
+  var hour2 = new Date(unix_time_2 * 1000);
+  var hour3 = new Date(unix_time_3 * 1000);
+  var hour4 = new Date(unix_time_4 * 1000);
+  var hour5 = new Date(unix_time_5 * 1000);
+  var hour6 = new Date(unix_time_6 * 1000);
+  var hour7 = new Date(unix_time_7 * 1000);
+  var hour8 = new Date(unix_time_8 * 1000);
+  var hour9 = new Date(unix_time_9 * 1000);
+  var hour10 = new Date(unix_time_10 * 1000);
+  var hour11= new Date(unix_time_11 * 1000);
+
+  hour0 = format(hour0);
+  hour1 = format(hour1);
+  hour2 = format(hour2);
+  hour3 = format(hour3);
+  hour4 = format(hour4);
+  hour5 = format(hour5);
+  hour6 = format(hour6);
+  hour7 = format(hour7);
+  hour8 = format(hour8);
+  hour9 = format(hour9); 
+  hour10 = format(hour10); 
+  hour11 = format(hour11); 
+  // Set Data
+  var data = google.visualization.arrayToDataTable([
+      ['Time', 'Temperature', { role: 'style' }],
+      [hour0,Math.floor(dat.hourly[0].temp),'color:black'],[hour1,Math.floor(dat.hourly[1].temp),'color:black'],
+      [hour2,Math.floor(dat.hourly[2].temp),'color:black'],[hour3,Math.floor(dat.hourly[3].temp),'color:black'],
+      [hour4,Math.floor(dat.hourly[4].temp),'color:black'],[hour5,Math.floor(dat.hourly[5].temp),'color:black'],
+      [hour6,Math.floor(dat.hourly[6].temp),'color:black'],[hour7,Math.floor(dat.hourly[7].temp),'color:black'],
+      [hour8,Math.floor(dat.hourly[8].temp),'color:black'],[hour9,Math.floor(dat.hourly[9].temp),'color:black'],
+      [hour10,Math.floor(dat.hourly[10].temp),'color:black'],[hour11,Math.floor(dat.hourly[11].temp),'color:black']
+  ]);
+  // Set Options
+  var options = {
+      title: 'time vs. temperature',
+      hAxis: {title: 'Time in Hours'},
+      vAxis: {title: 'Temperature in °C'},
+      legend: 'none',
+      tooltip: {isHtml: true},
+      backgroundColor: 'transparent',
+      color : 'black',
+      is3D : true,
+      allowHtml: true,
+  };
+  // Draw
+  document.querySelector("#myChart").style.display = "block";
+  var chart = new google.visualization.AreaChart(document.getElementById('myChart'));
+  chart.draw(data, options);
+}
+function format(date) {
+  var hours = date.getHours();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  var strTime = hours + ' ' + ampm;
+  return strTime;
+}
+window.onresize = (function(){
+  drawChart();
+});
 
 
 
