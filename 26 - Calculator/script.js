7fa4c844ea13cab74e816ae50be9c7ff21ffb362
Shaurya Kushwaha <shaurya08k@gmail.com>
2022-10-24 14:49:36 +0530

    Added input from keyboard

--- 26 - Calculator/script.js
+++ 26 - Calculator/script.js
@@ -73,3 +73,28 @@ buttons.map(button => {
     }
   });
 });
+document.addEventListener('keyup', (e) => {
+  if(((e.which >= 48 && e.which <= 57) || (e.which>= 96 && e.which <= 111) || (e.which >= 189 && e.which <= 191)) && !e.shiftKey){
+    display.innerText += e.key;
+  }
+  if(e.shiftKey && (e.which == 187 || e.which == 48 || e.which == 56 || e.which == 57 || e.which == 53)){
+    display.innerText += e.key;
+  }
+  if(e.which == 54 && e.shiftKey){
+    display.innerText += "**";
+  }
+  if(e.which == 13){
+    try {
+      display.innerText = eval(display.innerText);
+    }
+    catch {
+      display.innerText = "Error"
+    }
+  }
+  if(e.which == 8){
+    display.innerText = display.innerText.slice(0, -1);
+  }
+  if(e.which == 46){
+    display.innerText = "";
+  }
+})
