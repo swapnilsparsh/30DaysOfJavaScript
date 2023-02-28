604e3144a87d8fd4f8d3c7aaed06d902205dd972
ananyakushal <ananyakushal15@gmail.com>
2023-02-28 20:07:47 +0530

    ADDED LOADER AND POP-UP TO SAVE THE IMAGE
    I have added loader which will be visible till the data is fetched. I have also added the pop up to save the image instead of just opening the image in another tab.

--- 74 - Random Anime Profile/script.js
+++ 74 - Random Anime Profile/script.js
@@ -3,26 +3,39 @@ const animeBox = document.querySelector(".anime-box");
 const anime = document.querySelector(".anime");
 const animeName = document.querySelector(".hero-name");
 const animeLoader = document.querySelector(".anime-loader");
+const saveBTN = document.getElementById("save");
 
 const api_url = `https://api.catboys.com/img`;
 
 getBTN.addEventListener("click", async function () {
   animeLoader.style.display = "block";
+  try{
   const response = await fetch(api_url);
   const data = await response.json();
   anime.src = data.url;
-  animeBox.style.display = "block";
   animeName.textContent = data.artist;
   saveBTN.style.display = "inline-block";
+  animeBox.style.display = "block";
+  await new Promise((resolve) => setTimeout(resolve, 500));
   animeLoader.style.display = "none";
+  } 
+  catch (error) {
+    console.log(error);
+    animeLoader.style.display = "none";
+  }
 });
 
-const saveBTN = document.getElementById("save");
+
 saveBTN.addEventListener("click", function() {
   const link = document.createElement("a");
   link.href = anime.src;
   link.download = "anime.jpg";
   document.body.appendChild(link);
+  const confirmed = confirm("Do you want to download this image?");
+  if (confirmed) {
+    link.click();
+  }
   link.click();
   document.body.removeChild(link);
+  // confirm("Do you want to download this image?");
 });
