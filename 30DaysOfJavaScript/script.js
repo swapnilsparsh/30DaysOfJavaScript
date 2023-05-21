594fe28a01caa34b09bff0a067964cf44eacc145
Marin <marin.muktic3@gmail.com>
2023-05-21 18:23:36 +0200

    pagination buttons, tracking site, new functions

--- 30DaysOfJavaScript/script.js
+++ 30DaysOfJavaScript/script.js
@@ -1,15 +1,16 @@
 // Preloader
 var preloader = document.getElementById('loading');
-    
-        function loading(){
-           setTimeout( myfunction,2000);
-       }      
-       function myfunction(){
-        preloader.style.display = 'none';
-       }
+
+function loading() {
+  setTimeout(myfunction, 2000);
+}
+function myfunction() {
+  preloader.style.display = 'none';
+}
 
 
 $(document).ready(function () {
+  renderProjects()
   $(window).scroll(function () {
     if ($(this).scrollTop() >= 100) {
       $(".scrollToTop").fadeIn();
@@ -22,6 +23,7 @@ $(document).ready(function () {
     $("html, body").animate({ scrollTop: 0 }, 500);
     return false;
   });
+
 });
 
 function toggleDarkLight() {
@@ -51,17 +53,77 @@ function toggleDarkLight() {
   }
 }
 
-function search_project() {
-  let input = document.getElementById('searchbar').value;
-  input = input.toLowerCase();
-  var x = document.getElementsByClassName('item');
+let currentPage = 1;
 
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].querySelector('a').innerHTML.toLowerCase().includes(input)) {
+var x = document.getElementsByClassName('item');
+
+function search_project() {
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  
+  for (let i = 0; i < x.length; i++) {
+    const projectName = x[i].querySelector("h4").innerHTML.toLowerCase();
+
+    if (!projectName.includes(input)) {
       x[i].style.display = "none";
-    }
-    else {
+    } else {
       x[i].style.display = "list-item";
     }
   }
-}  
+}
+
+function renderProjects() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  const projectsPerPage = 30;
+  const startIndex = (currentPage - 1) * projectsPerPage;
+  const endIndex = startIndex + projectsPerPage;
+
+  for (let i = 0; i < x.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      x[i].style.display = "list-item";
+    } else {
+      x[i].style.display = "none";
+    }
+  }
+}
+
+function goToPage(page) {
+  currentPage = page;
+  renderProjects();
+
+  const buttons = document.querySelectorAll(".pagination button");
+  buttons.forEach((button) => {
+    if (button.textContent == currentPage) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+}
+
+function goToNextPage() {
+  if (currentPage < Math.ceil(x.length / 30)) {
+    currentPage++;
+    renderProjects();
+    updateActiveButton();
+  }
+}
+
+function goToPreviousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    renderProjects();
+    updateActiveButton();
+  }
+}
+
+
+function updateActiveButton() {
+  const buttons = document.querySelectorAll(".pagination button");
+  buttons.forEach((button) => {
+    if (parseInt(button.textContent) === currentPage) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+}
