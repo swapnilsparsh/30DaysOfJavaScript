95c4fbd738f9060b1de793aad9b61624d48aab2e
subhamkumarr <kumarsubham75861@gmail.com>
2023-07-12 13:31:44 +0530

    BMI calculator

--- 111 - BMI Calculator/index.js
+++ 111 - BMI Calculator/index.js
@@ -1,7 +1,56 @@
-function calc() {
-    let num1 = Number(document.querySelector("#num1").value); // reads the value of the input type text
-    let num3 = Number(document.querySelector("#num3").value);
-    let sum = num1*10000 / ((num3*num3) / 100*100);
-    document.getElementById("sum").value = sum.toFixed(2); 
-  // changes the value of type text in input tag
+var cal_btn=document.querySelector('.calculate_btn');
+var reset_btn=document.querySelector('.reset_btn');
+
+/*calculation function for BMI Value*/
+function BMI(){
+    var weight=document.querySelector('#weight').value;
+    var height_feet=document.querySelector('#height_feet').value; 
+    var height_inch=document.querySelector('#height_inch').value; 
+    var converted_height=((((parseInt(height_feet)*12)+parseInt(height_inch))*2.54)/100);
+    var new_converted_height=converted_height*converted_height;
+    var bmi_value=(weight/new_converted_height);
+    return bmi_value=bmi_value.toFixed(2);
+    // console.log(bmi_value);
 }
+
+/*functionality of calculate button*/
+cal_btn.addEventListener("click",()=>{
+    var bmi=document.querySelector('.BMI_Result');
+    var bmi_value=document.querySelector('.BMI_Value');
+    var result=BMI();
+    bmi_value.style.display='block';
+    bmi.style.display='block';
+    bmi_value.innerText="BMI Value is:"+result;
+     if(result<18.5){
+         bmi.innerText="You are Underweight";
+      }
+      else if(result>=18.5 && result<=24.9){
+        bmi.innerText="You are Normal";
+      }
+      else if(result>25.0 && result<=29.9){
+        bmi.innerText="You are Overweight";
+      }  
+      else if(result>=30 && result<=34.9){
+        bmi.innerText="You are Obese(class I)";
+      }
+      else if(result>=35 && result<=39.9){
+        bmi.innerText="You are Obese(Class II)";
+      }
+      else if(result>=40){
+        bmi.innerText="You are Obese(CLass III)";
+      }
+      
+    
+    
+});
+
+/*Functionality of Reset Button*/
+reset_btn.addEventListener("click",()=>{
+  var bmi=document.querySelector('.BMI_Result');
+  var bmi_value=document.querySelector('.BMI_Value');
+  document.querySelector('.weight_class').value='';
+  document.querySelector('.height_class').value='';
+  document.querySelector('#height_inch').value = ''; 
+  bmi_value.style.display='none';
+  bmi.style.display='none';
+});
