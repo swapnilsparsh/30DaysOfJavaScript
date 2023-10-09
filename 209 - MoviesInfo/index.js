f3cbed53dea3f582e9ff5d5b54e79e46d79abb23
AckermanLevi1 <aryan.bhargava.mec21@itbhu.ac.in>
2023-10-10 00:16:24 +0530

    MovieApp added

---
+++ 209 - MoviesInfo/index.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.getElementById("searchButton");
+    const movieNameInput = document.getElementById("movieName");
+    const movieInfo = document.getElementById("movieInfo");
+
+    searchButton.addEventListener("click", () => {
+        const movieName = movieNameInput.value.trim();
+        if (movieName === "") {
+            alert("Please enter a movie name.");
+            return;
+        }
+
+        // You can replace 'YOUR_OMDB_API_KEY' with your actual OMDB API key
+        const apiKey = "adff2bf8";
+        const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(movieName)}&apikey=${apiKey}`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Response === "True") {
+                    const movieTitle = data.Title;
+                    const movieYear = data.Year;
+                    const moviePlot = data.Plot;
+                    const movieimdb = data.imdbRating;
+                    movieInfo.innerHTML = `
+                        <h2>${movieTitle} (${movieYear})</h2>
+                        <p>${moviePlot}</p>
+                        <p>IMDB Rating is ${movieimdb}</p>
+                    `;
+                } else {
+                    movieInfo.innerHTML = "<p>Movie not found.</p>";
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    });
+});
