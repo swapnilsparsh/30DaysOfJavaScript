a7a950b24de30ec56c843620ad517a7a6dabd4e5
Nabin Acharya <nabinacharya740@gmail.com>
2024-01-27 11:39:23 +0545

    productive app

---
+++ 112 - Productive App/README.md
@@ -0,0 +1,35 @@
+# Productive App
+
+Features:
+
+1. **Time**
+- start
+- stop
+- reset
+
+2. **To-do list**
+- Add Task
+- Delete Task
+- Complete Task
+
+|![img](images/img1.png)|
+|---|
+
+---
+### 4. Notes section
+- Add Notes
+- Delete Notes
+- Double click remove note
+
+|![img](images/img2.png)|
+|---|
+---
+### 4. Light Theme
+|![img](images/light.png)|
+|---|
+
+---
+
+### 5. Dark Theme
+|![img](images/dark.png)|
+|---|
---
+++ 112 - Productive App/index.js
@@ -0,0 +1,215 @@
+//theme changer
+let theme=document.querySelector("#theme");
+let body=document.querySelector("body");
+let noteText=document.querySelector(".note");
+let currentMode="light";
+
+let changer=()=>{
+  if(currentMode==="light"){
+      currentMode="dark";
+      theme.innerText="Light Mode";
+      body.style.background="linear-gradient(to left,#101010,black)";
+      theme.style="background:linear-gradient(to left, lightblue, lightgreen);color:black";
+  }else{
+      currentMode="light";
+      theme.innerText="Dark Mode";
+      body.style.background="linear-gradient(to left, lightblue, lightgreen)";
+      theme.style="#theme";
+      
+  }
+};
+theme.addEventListener("click",changer);
+//note app
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
+
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
+
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+
+  return element;
+}
+
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note)=>note.id != id)
+    saveNote(notes)
+    appEl.removeChild(element)
+}
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  saveNote(notes);
+}
+
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
+
+const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const stopButtonEl = document.getElementById("stop");
+const resetButtonEl = document.getElementById("reset");
+
+
+//timer
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timerEl.textContent = formatTime(elapsedTime);
+  }, 10);
+
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
+}
+// console.log(startTimer());
+
+function formatTime(elapsedTime) {
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+  
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+function resetTimer() {
+  clearInterval(timerInterval);
+
+  elapsedTime = 0;
+  timerEl.textContent = "00:00:00";
+
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+
+startButtonEl.addEventListener("click", startTimer);
+stopButtonEl.addEventListener("click", stopTimer);
+resetButtonEl.addEventListener("click", resetTimer);
+
+
+//to do list code
+const formEl = document.querySelector(".form");
+
+const inputEl = document.querySelector(".input");
+
+const ulEl = document.querySelector(".list");
+
+let list = JSON.parse(localStorage.getItem("list"));
+if (list) {
+  list.forEach((task) => {
+    toDoList(task);
+  });
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  toDoList();
+});
+
+function toDoList(task) {
+  let newTask = inputEl.value;
+  if (task) {
+    newTask = task.name;
+  }
+
+  const liEl = document.createElement("li");
+  if (task && task.checked) {
+    liEl.classList.add("checked");
+  }
+  liEl.innerText = newTask;
+  ulEl.appendChild(liEl);
+  inputEl.value = "";
+  const checkBtnEl = document.createElement("div");
+  checkBtnEl.innerHTML = `
+  <i class="fas fa-check-square">
+  `;
+  liEl.appendChild(checkBtnEl);
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `
+  <i class="fas fa-trash"></i>
+  `;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
+    updateLocalStorage();
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+    updateLocalStorage();
+  });
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  const liEls = document.querySelectorAll("li");
+  list = [];
+  liEls.forEach((liEl) => {
+    list.push({
+      name: liEl.innerText,
+      checked: liEl.classList.contains("checked"),
+    });
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+}
