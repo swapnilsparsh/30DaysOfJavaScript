94388c6aed0412d493ab02cc1822bc8169f9aa20
douglas-kamseu <kamseudouglas@gmail.com>
2024-05-23 17:00:55 +0200

    Files Changes for Testing

--- 09 - Dice Roll/script.js
+++ 09 - Dice Roll/script.js
@@ -1,11 +1,14 @@
-function shuffle(){
+function shuffle() {
     const img = document.getElementById("img");
     let random = Math.floor(Math.random() * 6) + 1;
     img.setAttribute("src", `assets/${random}.png`);
 }
 
-function anim(){
+function anim() {
     setTimeout(shuffle, 500);
     const img = document.getElementById("img");
     img.setAttribute("src", "assets/dice-roll.gif");
 }
+
+setTimeout(shuffle, 500);
+const img = document.getElementById("img");
--- 10 - Emoji Switcher like Discord/script.js
+++ 10 - Emoji Switcher like Discord/script.js
@@ -4,14 +4,16 @@ const emojis = [];
 
 
 const emojiAddFunction = async () => {
-    
+
     let res = await fetch('https://emoji-api.com/emojis?access_key=1ce9b701f975ba7b63dd065ab1e09f26e3d1e83d')
     res = await res.json()
-    
-    for(let i=0 ; i<res.length ; i++){
+
+    for (let i = 0; i < res.length; i++) {
         emojis.push(res[i].character);
     }
 }
+
+emojiAddFunction();
 
 emojiAddFunction();
 
