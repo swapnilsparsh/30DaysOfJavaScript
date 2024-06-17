68ac3753843ef2be0a49bf95625e06494461e2c4
AsmitaMishra24 <146121869+AsmitaMishra24@users.noreply.github.com>
2024-06-18 00:47:53 +0530

    Added Flappy Bird

--- 30DaysOfJavaScript/script.js
+++ 30DaysOfJavaScript/script.js
@@ -189,7 +189,8 @@ function search_project() {
       { name: "Maze Game", url: "./112 - Maze Game/index.html" },
       { name: "Minesweeper", url: "./113 - Minesweeper/index.html" },
       { name: "Guess the Country", url: "./115 - Guess The Country/index.html"},
-      { name: "2048 Game", url: "./124 - 2048 Game/index.html"}
+      { name: "2048 Game", url: "./124 - 2048 Game/index.html"},
+      { name: "Flappy Bird Game", url: "./125 - Flappy Bird/index.html"}
 ];
 
   // Filter suggestions based on input
